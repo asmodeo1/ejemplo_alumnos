59d402795987a6f11b1518816adec8f537e0cee8
Juan <alonso.jad@gmail.com>
2024-05-22 12:25:17 +0200

    Add files via upload

---
+++ alumnos.js
@@ -0,0 +1,31 @@
+const nombres = ["eva", "luis", "sergio", "sandra", "eduardo", "ernesto", "sofia", "elena", "pablo"];
+const notas = [10, 5, 7, 3, 2, 7, 8, 5, 1];
+const cursos = [1, 2, 1, 2, 3, 3, 1, 2, 2];
+
+/**
+ * Muestra todos los alumnos en la sección alumnos
+ * @param {[String]} nombres - array con los nombres a mostrar
+ * @param {[Number]} notas - array con las notas a mostrar
+ * @param {[Number]} cursos - array con los cursos a mostrar
+ */
+function mostrarAlumnos(nombres, notas, cursos) {
+    const alumnos = document.getElementById('alumnos');
+    // Recorremos todos los arrays. Ponemos nombres.length porque todos los arrays
+    // tienen la misma longitud (podríamos poner notas.length o cursos.length)
+    for (let i = 0; i < nombres.length; i++) {
+        const div = document.createElement("div");
+        div.classList.add("alumno");
+        const spanNombre = document.createElement("span");
+        spanNombre.textContent = nombres[i];
+        div.appendChild(spanNombre);
+        const spanNota = document.createElement("span");
+        spanNota.textContent = notas[i];
+        div.appendChild(spanNota);
+        const spanCurso = document.createElement("span");
+        spanCurso.textContent = cursos[i];
+        div.appendChild(spanCurso);
+        alumnos.appendChild(div);
+    }
+}
+
+mostrarAlumnos(nombres, notas, cursos);
